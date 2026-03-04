4e4cc679bb19bec715d69b9a2e2f5cf8e8ff919e
cwitstudent <cwitstudent@CWIT-350-E06.vtc.hk>
2026-03-04 15:27:15 +0800

    top bars for each page

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -1,105 +1,81 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Collections.Generic;
-public class LevelData
-{
-    public int LevelIndex;
-    public string Title;
-}
 public class LevelCreator : MonoBehaviour
 {
     private VisualElement root;
     private VisualElement levelPage;
     private VisualElement contentRoot;
 
-    private List<LevelData> levels = new List<LevelData>();
-
-    // void GenerateLevelData()
-    // {
-    //     for (int i = 1; i <= 4; i++)
-    //     {
-    //         levels.Add(new LevelData
-    //         {
-    //             LevelIndex = i,
-    //             Title = "Level " + i
-    //         });
-    //     }
-    // }
-
-    // void SetupLevelButtons()
-    // {
-    //     for (int i = 0; i < levels.Count; i++)
-    //     {
-    //         var button = root.Q<Button>("LevelButton" + (i + 1));
-
-    //         int capturedIndex = i;
-
-    //         button.text = levels[i].LevelIndex.ToString();
-
-    //         button.clicked += () =>
-    //         {
-    //             OpenLevel(levels[capturedIndex]);
-    //         };
-    //     }
-    // }
-
-    // void OpenLevel(LevelData data)
-    // {
-    //     // Hide LevelPage
-    //     levelPage.EnableInClassList("ActivePage", false);
-
-    //     // Load MC Question UXML
-    //     VisualTreeAsset levelAsset =
-    //         Resources.Load<VisualTreeAsset>("UIDocuments/MCLevelPage");
-
-    //     VisualElement levelDetail = levelAsset.CloneTree();
-    //     levelDetail.name = "MCLevelPage";
-    //     levelDetail.AddToClassList("Page");
-    //     levelDetail.AddToClassList("ActivePage");
-
-    //     contentRoot.Add(levelDetail);
-
-    //     SetupLevelDetail(levelDetail, data);
-    // }
-
-    // void SetupLevelDetail(VisualElement levelDetail, LevelData data)
-    // {
-    //     var questionLabel = levelDetail.Q<Label>("QuestionLabel");
-    //     questionLabel.text = "What is 2 + 2?";
-
-    //     var optionA = levelDetail.Q<Button>("OptionA");
-    //     var optionB = levelDetail.Q<Button>("OptionB");
-    //     var optionC = levelDetail.Q<Button>("OptionC");
-
-    //     optionA.text = "3";
-    //     optionB.text = "4";
-    //     optionC.text = "5";
-
-    //     optionB.clicked += () =>
-    //     {
-    //         Debug.Log("Correct!");
-    //     };
-
-    //     var backButton = levelDetail.Q<Button>("BackButton");
-    //     backButton.clicked += () =>
-    //     {
-    //         levelDetail.RemoveFromHierarchy();
-    //         levelPage.EnableInClassList("ActivePage", true);
-    //     };
-    // }
+    private List<VisualElement> pages;
+    private List<Button> navButtons;
 
     private ScrollView scrollView;
+    private TopBarCreator topBarCreator;
 
     private int levelsPerCharacter = 4;
 
     void Start()
     {
+        topBarCreator = GetComponent<TopBarCreator>();
         root = GetComponent<UIDocument>().rootVisualElement;
         levelPage = root.Q<VisualElement>("LevelPage");
 
         scrollView = levelPage.Q<ScrollView>("Root-LevelList-ScrollView");
-
+        SetupPages();
+        InitBottomBar();
         GenerateLevelRows(5); // Create 5 character rows (20 levels total)
+    }
+
+    private void InitBottomBar()
+    {
+        VisualElement bottomBar = root.Q<VisualElement>("Root-BottomBar");
+
+        navButtons = bottomBar.Query<Button>().ToList();
+
+        foreach (var button in navButtons)
+        {
+            button.clicked += () => {
+                OnNavClicked(button);
+            };
+            Debug.Log("Found button: " + button.name);
+        }
+    }
+
+
+    void SetupPages()
+    {
+        pages = root.Query<VisualElement>(className: "Page").ToList();
+        foreach (var page in pages)
+        {
+            Debug.Log("Found page: " + page.name);
+        }
+    }
+
+    void OnNavClicked(Button clickedButton)
+    {
+        // Remove selected state from all nav buttons
+        foreach (var btn in navButtons)
+            btn.RemoveFromClassList("NavIconSelected");
+
+        // Add selected state to clicked button
+        clickedButton.AddToClassList("NavIconSelected");
+
+        // Determine which page to show
+        string targetPageName = clickedButton.name.Replace("Nav", "Page");
+
+        // Hide all pages
+        foreach (var page in pages)
+            page.RemoveFromClassList("ActivePage");
+
+        // Show target page
+        var targetPage = root.Q<VisualElement>(targetPageName);
+        if (targetPage != null)
+        {
+            targetPage.AddToClassList("ActivePage");
+            topBarCreator.ShowTopBar(targetPageName);
+        }
+            
     }
 
     void GenerateLevelRows(int characterCount)
---
+++ Assets/Scripts/TopBarCreator.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class TopBarCreator : MonoBehaviour
+{
+    private VisualElement root;
+    private VisualElement topBarContent;
+
+    void Start()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+        topBarContent = root.Q<VisualElement>("TopBarContent");
+        ShowTopBar("LevelPage");
+    }
+
+    public void ShowTopBar(string pageName)
+    {
+        switch (pageName)
+        {
+            case "LevelPage":
+            {
+                ReplaceTopBar("TopBars/LevelTopBar");
+                break;
+            }
+            case "AchievementPage":
+            {
+                ReplaceTopBar("TopBars/AchievementTopBar");
+                break;
+            }
+            case "ProfilePage":
+            {
+                ReplaceTopBar("TopBars/ProfileTopBar");
+                break;
+            }
+        }
+    }
+
+    void ReplaceTopBar(string resourcePath)
+    {
+        topBarContent.Clear();
+        string fullPath = "UIDocuments/" + resourcePath;
+        VisualTreeAsset asset =
+            Resources.Load<VisualTreeAsset>(fullPath);
+
+        if (asset != null)
+        {
+            VisualElement content = asset.CloneTree();
+            topBarContent.Add(content);
+        }
+    }
+}
